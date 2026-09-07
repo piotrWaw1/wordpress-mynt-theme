a685bfbf98f2287843777bb393338000a2cc4bcb
Piotr <piotrwc49@gmail.com>
2026-09-07 23:17:29 +0200

    facebook icon

--- template-parts/social-media-links.php
+++ template-parts/social-media-links.php
@@ -1,5 +1,6 @@
 <?php
   $instagram = get_field('instagram');
+  $facebook = get_field('facebook')
 ?>
 
 <?php if ( ! empty( $instagram['instagram_url'] ) && is_array( $instagram['instagram_url'] ) ) : ?>
@@ -14,6 +15,17 @@
             aria-label="<?php echo esc_attr( $instagram['instagram_url']['title'] ); ?>"
         >
             <?php echo $instagram['icon']; ?>
+        </a>
+        <a 
+            class="social-link" 
+            href="<?php echo esc_url( $facebook['facebook_url']['url'] ); ?>" 
+            target="<?php echo esc_attr( $facebook['facebook_url']['target'] ?: '_self' ); ?>"
+            <?php if ( $facebook['facebook_url']['target'] === '_blank' ) : ?>
+               rel="noopener noreferrer"
+            <?php endif; ?>
+            aria-label="<?php echo esc_attr( $facebook['facebook_url']['title'] ); ?>"
+        >
+            <?php echo $facebook['facebook_icon']; ?>
         </a>
     </div>
 <?php endif; ?>
