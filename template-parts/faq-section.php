396ce8a25a9c97f84c8e978daa8c85f269a20440
Piotr <piotrwc49@gmail.com>
2026-09-25 16:16:37 +0200

    faq space fix and button icon

--- template-parts/faq-section.php
+++ template-parts/faq-section.php
@@ -12,22 +12,24 @@ $description_faq = esc_html(get_field("description_faq"));
       <p><?php echo $description_faq ?></p>
     </div>
     <?php if ( have_rows('questions') ) : ?>
-        <?php while ( have_rows('questions') ) : the_row();
-            $question = esc_html(get_sub_field('question'));
-            $answer = esc_html(get_sub_field('answer'));
-        ?>
-            <div class="faq-list reveal-stagger" id="faqList">
-                <div class="faq-item">
-                    <button class="faq-question" aria-expanded="false">
-                        <span><?php echo $question?></span>
-                        <span class="faq-icon">+</span>
-                    </button>
-                    <div class="faq-answer">
-                        <p><?php echo $answer?></p>
+        <div class="faq-list reveal-stagger" id="faqList">
+            <?php while ( have_rows('questions') ) : the_row();
+                $question = esc_html(get_sub_field('question'));
+                $answer = esc_html(get_sub_field('answer'));
+            ?>
+                
+                    <div class="faq-item">
+                        <button class="faq-question" aria-expanded="false">
+                            <span><?php echo $question?></span>
+                            <span class="faq-icon"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up preview-icon"><path d="m5 12 7-7 7 7"/><path d="M12 19V5"/></svg></span>
+                        </button>
+                        <div class="faq-answer">
+                            <p><?php echo $answer?></p>
+                        </div>
                     </div>
-                </div>
-            </div>
-        <?php endwhile; ?>
+                
+            <?php endwhile; ?>
+        </div>
     <?php endif; ?>
     <p class="faq-note">Masz inne pytanie? <a href="#kontakt">Napisz do nas</a></p>
   </div>
